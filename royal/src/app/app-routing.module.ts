7bcbcf830a2aed54a215af828faf542f5da386dd
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-11-27 14:59:11 +0200

    Modified

--- royal/src/app/app-routing.module.ts
+++ royal/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CartComponent } from './cart/cart.component'
+
+const routes: Routes = [
+  { path: '/cart', component: CartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
