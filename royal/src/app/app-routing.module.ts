2e2bb2d2c55723e9aff01299585f2e33e500b8ad
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-11-27 15:30:00 +0200

    Modified

--- royal/src/app/app-routing.module.ts
+++ royal/src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CartComponent } from './cart/cart.component'
+import { LoginComponent } from './login/login.component'
+import { ProductComponent } from './product/product.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  { path: '/cart', component: CartComponent},
+  { path: 'cart', component: CartComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '', component: ProductComponent},
+  { path: 'user', component: UserComponent}
 ];
 
 @NgModule({
