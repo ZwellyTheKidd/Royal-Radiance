65cdc3db069277525b998747bcdc0cc9702080ef
ZwellyTheKidd <zwelly.thekidd@outlook.com>
2023-11-28 11:33:57 +0200

    landing

--- royal/src/app/app-routing.module.ts
+++ royal/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
+   { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'landing', component: ProductsComponent },
   { path: 'products/:id', component: ProductDetailComponent },
 ];
 
