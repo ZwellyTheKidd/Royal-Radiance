83df947ca0019ab861800cf296bf4beee8ef861f
Zwelly Sithole <87071853+ZwellyTheKidd@users.noreply.github.com>
2024-07-23 21:57:06 +0200

    Update index.js to royal-radiance.vercel

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:4200']
+    origin: ['https://royal-radiance.vercel.app']
 }));
 app.use(express.json());
 
